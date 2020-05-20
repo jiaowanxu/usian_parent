4e9dd1473f55ae9cc4bc38b1653f02ce9b0cae3d
jiaowanxu <wwq.yy@qq.com>
2020-05-21 00:47:58 +0800

    发布项目到github7.0

---
+++ usian_content_feign/src/main/java/com/usian/feign/ContentServiceFeign.java
@@ -0,0 +1,16 @@
+package com.usian.feign;
+
+import com.usian.pojo.TbContentCategory;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@FeignClient(value = "usian-content-service")
+public interface ContentServiceFeign {
+
+    @RequestMapping("/service/contentCategory/selectContentCategoryByParentId")
+    List<TbContentCategory> selectContentCategoryByParentId(@RequestParam Long id);
+
+}
